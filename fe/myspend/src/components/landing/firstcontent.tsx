5b8ef7f81d87c5c94379f22c03e6dda0ac3c9559
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-25 17:37:43 +0700

    [ADD] landing component and navbar component

---
+++ fe/myspend/src/components/landing/firstcontent.tsx
@@ -0,0 +1,106 @@
+import { Box, Button, Fade, Image, Text } from "@chakra-ui/react";
+import React, { useEffect, useMemo } from "react";
+import { fontFamily } from '../../styling/style';
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+
+const FirstContent = () => {
+
+    const font = 'Inter, sans-serif';
+
+    const textArray = [ 
+        'Manage your finance through one website',
+        'All your money in one place',
+        'Keep your budget update'
+    ];
+
+    const [index, setIndex] = React.useState<number>(0);
+
+    useEffect(() => {
+        if(index == 3){
+            setIndex(0);
+        }
+        setTimeout(() => setIndex(index + 1), 5000);  
+    }, [index]);
+
+    return(
+        <Box 
+        border='4px solid black'
+        background='linear-gradient(360deg, #EDE0F1 60.01%, #EDEFFB 103.33%)'
+        height='100%'
+        display='flex'
+        flexDir='row'>
+            <Box
+            position='relative'
+            width='50%'
+            border = '4px solid yellow'>
+                <Fade in>
+                    <Text
+                    fontFamily={{font}}
+                    fontWeight={600}
+                    color='#215C94'
+                    fontSize={{
+                        'lg' : '55px'
+                    }}
+                    position='absolute'
+                    top='30%'
+                    left='9%'
+                    width='70%'>
+                        {textArray[index]}
+                    </Text>
+                </Fade>
+                <Button
+                backgroundColor='#215C94'
+                position='relative'
+                top='80%'
+                left='10%'
+                width='30%'
+                rightIcon={<ArrowForwardIcon color='white'/>}>
+                    <Box
+                    width='60%'>
+                        <Text
+                        fontFamily={fontFamily}
+                        fontSize={{
+                            'lg' : '14px'
+                        }}
+                        fontWeight={600}
+                        color='white'>Get started</Text>
+                    </Box>
+                </Button>
+            </Box>
+            <Box
+            backgroundImage='./hp.jpg'
+            zIndex='2'
+            blendMode='multiply'
+            backgroundRepeat='no-repeat'
+            boxSize='45%'
+            backgroundSize='cover'
+            position='relative'
+            right='4%'>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+            </Box>
+        </Box>
+    )
+}
+
+export default FirstContent;
---
+++ fe/myspend/src/components/navbar/index.tsx
@@ -0,0 +1,58 @@
+import { Box, Flex, Image, Link, Text } from "@chakra-ui/react"
+import { fontFamily } from "../../styling/style";
+import { textSize } from "../../styling/textsize";
+const black = '#1A2127';
+
+const Navbar = () => {
+    return(
+        <Flex 
+        flexDir='row'
+        backgroundColor='#EDE0F1'>
+            <Box 
+            width='15%'
+            height='100%'
+            position='relative'
+            left='10%'>
+                <Link 
+                display='flex' 
+                flexDir='row'
+                alignItems='center'>
+                    <Image src='Logo.svg'></Image>
+                    <Text
+                    fontFamily={fontFamily}
+                    alignSelf='center'
+                    fontSize={textSize}
+                    color='#215C94'
+                    fontWeight='bold'>MySpend</Text>
+                </Link>
+            </Box>
+            <Box
+            position='relative'
+            left='30%'
+            display='flex'
+            flexDir='row'
+            gap={{
+                'lg' : '4rem'
+            }}
+            alignItems={'center'}
+            width='30%'>
+                <Link><Text
+                fontFamily={fontFamily}
+                fontSize={textSize}
+                color={black}>Features</Text></Link>
+
+                <Link><Text
+                fontFamily={fontFamily}
+                fontSize={textSize}
+                color={black}>FAQ</Text></Link>
+
+                <Link><Text
+                fontFamily={fontFamily}
+                fontSize={textSize}
+                color={black}>About</Text></Link>
+            </Box>
+        </Flex>
+    )
+}
+
+export default Navbar;
