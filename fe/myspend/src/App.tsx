52d3adadb8bff41d4930a9806f722010e90349a1
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-25 13:59:30 +0700

    Initial Commit

---
+++ fe/myspend/src/App.tsx
@@ -0,0 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+
+function App() {
+  return (
+    <ChakraProvider>
+      
+    </ChakraProvider>
+  );
+}
+
+export default App;
