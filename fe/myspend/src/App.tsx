1e99b7d418a45733087d252a3edcab7e5509e69b
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-25 17:36:05 +0700

    [ADD] chakra provider and browser router

--- fe/myspend/src/App.tsx
+++ fe/myspend/src/App.tsx
@@ -1,10 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <ChakraProvider>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
