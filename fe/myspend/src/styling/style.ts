8d1a4a12bd9a75354de7c1fd769e48d2e0c835ac
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-26 16:48:38 +0700

    [ADD] new universal styling

--- fe/myspend/src/styling/style.ts
+++ fe/myspend/src/styling/style.ts
@@ -1,1 +1,5 @@
-export const fontFamily = "'Nunito', sans-serif";
+export const fontFamilyNunito = "'Nunito', sans-serif";
+export const blue = '#215C94';
+export const black = '#1A2127';
+export const biruMuda = '#EDEFFB';
+export const fontFamilyInter = 'Inter, sans-serif';
