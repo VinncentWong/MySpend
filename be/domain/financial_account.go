cfac6c6877a0abef64d3b1b3bc23693e9a82bf72
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-28 17:32:18 +0700

    [ADD] financial account struct

---
+++ be/domain/financial_account.go
@@ -0,0 +1,11 @@
+package domain
+
+import "gorm.io/gorm"
+
+type FinancialAccount struct {
+	gorm.Model
+	Bankname      string `json:"bankname"`
+	Username      string `json:"username"`
+	Accountnumber string `json:"accountnumber"`
+	UserID        uint
+}
