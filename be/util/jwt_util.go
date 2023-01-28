07d70b3b6bfc40e08940f225db9b4693f56db110
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-28 17:34:43 +0700

    [ADD] jwt utilities

---
+++ be/util/jwt_util.go
@@ -0,0 +1,52 @@
+package util
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func CreateToken(data any) (*string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"data": data,
+	})
+	fixedToken, err := token.SignedString(os.Getenv("secret_key"))
+	if err != nil {
+		return nil, err
+	}
+	return &fixedToken, err
+}
+
+func ValidateToken(token string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("Authorization")
+		if token == "" {
+			c.Next()
+			return
+		}
+		if !strings.HasPrefix(token, "Bearer ") {
+			SendResponse(c, http.StatusForbidden, "unauthorized", false, nil)
+			return
+		} else {
+			fixToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+				return os.Getenv("secret_key"), nil
+			})
+			if err != nil {
+				SendResponse(c, http.StatusForbidden, "unauthorized", false, nil)
+				c.Abort()
+				return
+			}
+			_, ok := fixToken.Claims.(jwt.MapClaims)
+			if !ok {
+				SendResponse(c, http.StatusForbidden, "wrong claims", false, nil)
+				c.Abort()
+				return
+			} else {
+				c.Next()
+			}
+		}
+	}
+}
