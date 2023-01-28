80c34eb0e3fba3c71203d635c2cf54677769c6e0
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-28 17:38:11 +0700

    [ADD] new code for starting server

--- be/main.go
+++ be/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"module/app/user/handler"
+	"module/app/user/repository"
+	"module/app/user/usecase"
+	"module/infrastructure"
+	"module/rest"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// Load Env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("can't load env properties, %s", err.Error())
+		return
+	}
+
+	err = infrastructure.InitDb()
+	if err != nil {
+		log.Fatal("exception on database initialization")
+		return
+	}
+	err = infrastructure.Migrate()
+	if err != nil {
+		log.Fatal("exception when migrating")
+		return
+	}
+
+	// Routing Initialization
+	r := gin.Default()
+
+	// User App
+	userRepo := repository.NewUserRepository()
+	userService := usecase.NewUserService(userRepo)
+	userHandler := handler.NewUserHandler(userService)
+
+	rest.UserRouting(r, userHandler)
+
+	r.Run()
 }
