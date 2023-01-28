4e35cc31854dbe993fc7f8c09d94c7d28aaa00e8
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-28 17:32:42 +0700

    [ADD] payment history struct

---
+++ be/domain/payment_history.go
@@ -0,0 +1,17 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type PaymentHistory struct {
+	ID               uuid.UUID `json:"id"`
+	BankName         string    `json:"bankname"`
+	Price            uint      `json:"price"`
+	LinkPaymentProof string    `json:"linkpaymentproof"`
+	CreatedAt        time.Time `gorm:"autoCreateTime"`
+	UpdatedAt        time.Time `gorm:"autoUpdateTime"`
+	UserID           uint
+}
