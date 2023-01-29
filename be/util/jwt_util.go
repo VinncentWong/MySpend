81f850d8da02c32e4c2848d51627ba4a132c070b
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-29 18:05:36 +0700

    [CHANGE] secret key data type

--- be/util/jwt_util.go
+++ be/util/jwt_util.go
@@ -13,7 +13,7 @@ func CreateToken(data any) (*string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"data": data,
 	})
-	fixedToken, err := token.SignedString(os.Getenv("secret_key"))
+	fixedToken, err := token.SignedString([]byte(os.Getenv("secret_key")))
 	if err != nil {
 		return nil, err
 	}
