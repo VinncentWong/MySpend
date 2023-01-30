bc69d980acabcc3c00674c281c36a1478b8dc583
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-30 21:43:27 +0700

    [REMOVE] validate token

--- be/util/jwt_util.go
+++ be/util/jwt_util.go
@@ -1,11 +1,8 @@
 package util
 
 import (
-	"net/http"
 	"os"
-	"strings"
 
-	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
 
@@ -18,35 +15,4 @@ func CreateToken(data any) (*string, error) {
 		return nil, err
 	}
 	return &fixedToken, err
-}
-
-func ValidateToken(token string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		token := c.Request.Header.Get("Authorization")
-		if token == "" {
-			c.Next()
-			return
-		}
-		if !strings.HasPrefix(token, "Bearer ") {
-			SendResponse(c, http.StatusForbidden, "unauthorized", false, nil)
-			return
-		} else {
-			fixToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
-				return os.Getenv("secret_key"), nil
-			})
-			if err != nil {
-				SendResponse(c, http.StatusForbidden, "unauthorized", false, nil)
-				c.Abort()
-				return
-			}
-			_, ok := fixToken.Claims.(jwt.MapClaims)
-			if !ok {
-				SendResponse(c, http.StatusForbidden, "wrong claims", false, nil)
-				c.Abort()
-				return
-			} else {
-				c.Next()
-			}
-		}
-	}
 }
