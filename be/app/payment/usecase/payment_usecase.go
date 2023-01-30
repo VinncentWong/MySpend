09b4afd36069d451fa3e4363ce13ca75fc2780af
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-30 21:42:36 +0700

    [ADD] payment usecase

---
+++ be/app/payment/usecase/payment_usecase.go
@@ -0,0 +1,30 @@
+package usecase
+
+import (
+	"mime/multipart"
+	"module/app/payment/repository"
+	"module/domain"
+	"module/dto/payment"
+)
+
+type PaymentService struct {
+	repo *repository.PaymentRepository
+}
+
+func NewPaymentService(r *repository.PaymentRepository) *PaymentService {
+	return &PaymentService{
+		repo: r,
+	}
+}
+
+func (s *PaymentService) CreatePaymentHistory(id string, dto payment.CreatePayment, file *multipart.FileHeader) (*domain.PaymentHistory, error) {
+	payment := domain.PaymentHistory{
+		BankName: dto.Bankname,
+		Price:    dto.TotalPayment,
+	}
+	result, err := s.repo.CreatePaymentHistory(id, payment, file)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
