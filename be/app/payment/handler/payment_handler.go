e6e3fa3ac52a263067a08c3f544d4c26e5e6194c
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-30 21:43:00 +0700

    [ADD] payment handler

---
+++ be/app/payment/handler/payment_handler.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"module/app/payment/usecase"
+	"module/dto/payment"
+	"module/util"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/goccy/go-json"
+)
+
+type PaymentHandler struct {
+	s *usecase.PaymentService
+}
+
+func NewPaymentHandler(us *usecase.PaymentService) *PaymentHandler {
+	return &PaymentHandler{
+		s: us,
+	}
+}
+
+func (h *PaymentHandler) CreatePaymentHistory(c *gin.Context) {
+	id := c.Param("id")
+	form, err := c.MultipartForm()
+	if err != nil {
+		util.SendResponse(c, http.StatusInternalServerError, err.Error(), false, nil)
+		return
+	}
+	formData := form.Value["data"]
+	var container payment.CreatePayment
+	err = json.Unmarshal([]byte(formData[0]), &container)
+	if err != nil {
+		util.SendResponse(c, http.StatusInternalServerError, err.Error(), false, nil)
+		return
+	}
+	err = validator.New().Struct(&container)
+	if err != nil {
+		errs := err.(validator.ValidationErrors)
+		util.SendResponse(c, http.StatusBadRequest, errs.Error(), false, nil)
+		return
+	}
+	files := form.File["photo"]
+	file := files[0]
+	result, err := h.s.CreatePaymentHistory(id, container, file)
+	if err != nil {
+		util.SendResponse(c, http.StatusInternalServerError, err.Error(), false, nil)
+		return
+	}
+	util.SendResponse(c, http.StatusOK, "success create payment handler", true, result)
+}
