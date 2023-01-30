287700e1a7576ec618fdae50cb0183c5289f4709
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-30 21:38:47 +0700

    [ADD] financial and payment implementation also routing

--- be/main.go
+++ be/main.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"log"
+	fHandler "module/app/financial_account/handler"
+	fRepo "module/app/financial_account/repository"
+	fUsecase "module/app/financial_account/usecase"
+	pHandler "module/app/payment/handler"
+	pRepo "module/app/payment/repository"
+	pUsecase "module/app/payment/usecase"
 	"module/app/user/handler"
 	"module/app/user/repository"
 	"module/app/user/usecase"
@@ -35,17 +41,33 @@ func main() {
 
 	// Routing Initialization
 	r := gin.Default()
+
+	// Use cors
 	r.Use(cors.New(cors.Config{
 		AllowAllOrigins: true,
 		AllowMethods:    []string{"*"},
 		AllowHeaders:    []string{"*"},
 	}))
+
 	// User App
 	userRepo := repository.NewUserRepository()
 	userService := usecase.NewUserService(userRepo)
 	userHandler := handler.NewUserHandler(userService)
 
+	// Financial Account App
+	financialAccountRepo := fRepo.NewFinancialAccountRepository()
+	financialAccountService := fUsecase.NewFinancialAccountService(financialAccountRepo)
+	financialAccountHandler := fHandler.NewFinancialAccountHandler(financialAccountService)
+
+	// Payment History App
+	paymentRepo := pRepo.NewPaymentRepository()
+	paymentService := pUsecase.NewPaymentService(paymentRepo)
+	paymentHandler := pHandler.NewPaymentHandler(paymentService)
+
 	rest.UserRouting(r, userHandler)
+	rest.FinancialAccountRouting(r, financialAccountHandler)
+	rest.CheckHealthRouting(r)
+	rest.PaymentHistoryRouting(r, paymentHandler)
 
 	r.Run(":8000")
 }
