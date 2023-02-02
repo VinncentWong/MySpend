a9d387662b83292a2e967ceb306a87d1c5714e6e
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-02 21:29:36 +0700

    [ADD] cache configuration

---
+++ be/infrastructure/cache.go
@@ -0,0 +1,31 @@
+package infrastructure
+
+import (
+	"time"
+
+	cache "github.com/go-redis/cache/v8"
+	rv8 "github.com/go-redis/redis/v8"
+)
+
+var redisCache *cache.Cache
+
+func InitCache() error {
+	ring := rv8.NewRing(&rv8.RingOptions{
+		Addrs: map[string]string{
+			"redis": "localhost:6379",
+		},
+		DB: 0,
+	})
+
+	_redisCache := cache.New(&cache.Options{
+		Redis:      ring,
+		LocalCache: cache.NewTinyLFU(1000, 5*time.Minute),
+	})
+
+	redisCache = _redisCache
+	return nil
+}
+
+func GetCache() *cache.Cache {
+	return redisCache
+}
