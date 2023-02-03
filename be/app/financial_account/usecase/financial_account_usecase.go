9a9026fe8d2d4f1803d291e50409163e684d765f
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-03 17:58:51 +0700

    [ADD] financial account usecase

---
+++ be/app/financial_account/usecase/financial_account_usecase.go
@@ -0,0 +1,33 @@
+package usecase
+
+import (
+	"module/app/financial_account/repository"
+	"module/domain"
+	"module/dto/financial_account"
+)
+
+type FinancialAccountService struct {
+	repository repository.IFinancialAccountRepository
+}
+
+func NewFinancialAccountService(r repository.IFinancialAccountRepository) *FinancialAccountService {
+	return &FinancialAccountService{
+		repository: r,
+	}
+}
+
+func (s *FinancialAccountService) CreateFinancialAccount(id string, dto financial_account.CreateFinancialAccountDto) (*domain.FinancialAccount, error) {
+	result, err := s.repository.CreateFinancialAccount(id, dto)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (s *FinancialAccountService) GetFinancialAccount(id string) (*[]domain.FinancialAccount, error) {
+	result, err := s.repository.GetFinancialAccount(id)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
