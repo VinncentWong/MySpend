6a5140a212db10f45e13cc0f36a4744276c7359d
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-04 14:34:16 +0700

    [CHANGE] token validation algorithm

--- be/util/token_util.go
+++ be/util/token_util.go
@@ -3,35 +3,39 @@ package util
 import (
 	"context"
 	"fmt"
+	"log"
 	"module/domain"
 	"module/infrastructure"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
+	"gorm.io/gorm"
 )
 
 func GenerateNewAccessToken(c *gin.Context) {
 	token := c.Request.Header.Get("access-token")
 	refreshToken := c.Request.Header.Get("refresh-token")
-
-	if strings.HasPrefix(token, "Bearer ") {
-		SendResponse(c, http.StatusBadRequest, "invalid token", false, nil)
+	accessToken, _ := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("secret_key")), nil
+	})
+	exp := accessToken.Claims.(jwt.MapClaims)["exp"].(float64)
+	log.Default().Printf("now = %v, exp = %v", time.Now().Unix(), int64(exp))
+	if int64(exp) > time.Now().Unix() {
+		SendResponse(c, http.StatusUnauthorized, "token still valid", false, nil)
 		return
 	}
 	jwtToken, err := jwt.Parse(refreshToken, func(t *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("secret_key")), nil
 	})
 	if err != nil {
-		SendResponse(c, http.StatusBadRequest, err.Error(), false, nil)
-		return
-	}
-	if jwtToken.Valid {
-		SendResponse(c, http.StatusBadRequest, "token still valid", false, nil)
-		return
+		log.Default().Printf("refresh token with error %v", err.Error())
+		if err.Error() != "Token is expired" {
+			SendResponse(c, http.StatusUnauthorized, err.Error(), false, nil)
+			return
+		}
 	}
 	claims, ok := jwtToken.Claims.(jwt.MapClaims)
 	if !ok {
@@ -42,9 +46,13 @@ func GenerateNewAccessToken(c *gin.Context) {
 	name := claims["name"].(string)
 	email := claims["email"].(string)
 	user := domain.User{
+		Model: gorm.Model{
+			ID: uint(id),
+		},
 		Name:  name,
 		Email: email,
 	}
+	log.Default().Printf("id = %v", id)
 	newToken, err := CreateToken(user)
 	if err != nil {
 		SendResponse(c, http.StatusInternalServerError, err.Error(), false, nil)
